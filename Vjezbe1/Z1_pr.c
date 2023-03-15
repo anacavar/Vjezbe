2e31b78debae17c086fbd573caf7c906cbb72ca0
Ana Cavar <ana.cavar22@gmail.com>
2023-03-15 09:45:53 +0100

    Z1 primjer

--- Vjezbe1/Z1_pr.c
+++ Vjezbe1/Z1_pr.c
@@ -1,61 +1,31 @@
 #include <stdio.h>  // standardne ulazno, izlazne funkcije
 #include <stdlib.h> // standardne C-funkcije
 #include <math.h>   // potrebne matematicke funkcije
-
-#include "ran1.c" // ucitavamo datoteku u kojoj je definiran generator
+#include "ran1.c"   // ucitavamo datoteku u kojoj je definiran generator
 
 int main(void)
 {
-  unsigned long i;
-  float r;
-  float a, b, c;
-  double s = 0.0; // suma
-  float res;
-  long idum = (-2578); // RAN: generatoru u pocetku prosljedjujemo negativni cijeli broj
-  FILE *fin, *fout;
-  fout = fopen("Z1_pr.txt", "w"); // datoteka za pohranu
-  for (i = 0; i < 1e8; i++)
+  int i;
+  float x, y;
+  float pi;
+  int unutar, ukupno;
+  float udaljenost;
+  long idum1, idum2;
+  idum1 = -1234;
+  idum2 = -4321;
+  ukupno = 10000;
+  unutar = 0;
+  for (i = 0; i < ukupno; i++)
   {
-    r = ran1(&idum); // RAN: pozivamo gnereator i pohranjujemo generirani broj u varijablu r
-
-    if (i == 0)
+    x = ran1(&idum1);
+    y = ran1(&idum2);
+    udaljenost = sqrt((x - 0.5) * (x - 0.5) + (y - 0.5) * (y - 0.5));
+    if (udaljenost < 0.5)
     {
-      a = r;
-    }
-    if (i == 1)
-    {
-      b = r;
-    }
-    if (i == 2)
-    {
-      c = r;
-    }
-    // suma
-    if (i > 2)
-    {
-      if (i % 3 == 0)
-      {
-        s += a * r;
-        a = r;
-      }
-      if (i % 3 == 1)
-      {
-        s += b * r;
-        b = r;
-      }
-      if (i % 3 == 0)
-      {
-        s += b * r;
-        b = r;
-      }
-    }
-
-    if (i % 1000 == 500)
-    {
-      res = fabs(((s / (i - 2)) - 0.25) * sqrt(i - 2));
-      fprintf(fout, "%7lu  %13.8e\n", i, res); // ispis u datoteku
+      unutar++;
     }
   }
-
+  pi = 4 * ((float)unutar / (float)ukupno);
+  printf("pi = %f", pi);
   return 0;
 }
