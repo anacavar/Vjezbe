998c318db9fab12a92fc88f46f4eab9cb5cd6fed
Ana Cavar <ana.cavar22@gmail.com>
2023-04-23 19:18:35 +0200

    fix

--- Vjezbe2/Z2c.c
+++ Vjezbe2/Z2c.c
@@ -3,8 +3,8 @@
 #include <math.h>
 #include "ran1.c"
 
-// Podijelite Ω na kvadratnu mrežu i odredite vjerojatnost P da se cestica na ˇ de u pojedinoj ¯ celiji ´
-// mreže. Analizirajte ponašanje sustava crtajuci razdiobe ´ Nw = 250 000 šetaca za nekoliko ˇ
+// Podijelite Ω na kvadratnu mrežu i odredite vjerojatnost P da se cestica nađe u pojedinoj ćeliji
+// mreže. Analizirajte ponašanje sustava crtajući razdiobe Nw = 250 000 šetaća za nekoliko
 // odabranih k = t/∆t
 
 // mreža je od 0 do 100 u x i u y smjeru
@@ -13,12 +13,15 @@ int main(void)
 {
 	int i, j;
 	long idum = -1234;
-	float x[250000];
-	float y[250000];
-	float P[1000]; // 100 * 100 kockica
-	int Nw = 250000;
-	int Nk = 5000;
-	int k = 100; // k = t / Dt
+	// float x[250000]; // x koordinata svakog šetača
+	float x[10]; // x koordinata svakog šetača
+	// float y[250000]; // y koordinata svakog šetača
+	float y[10]; // y koordinata svakog šetača
+	// int Nw = 250000; // broj šetača
+	int Nw = 10; // broj šetača
+	// int Nk = 5000; // ŠTO ĆE MI OVAJ? broj koraka?
+	int Nk = 1000; // ŠTO ĆE MI OVAJ? broj koraka?
+	int k = 100;	 // k = t / Dt
 
 	FILE *file;
 	file = fopen("Z2c.txt", "w");
@@ -26,33 +29,39 @@ int main(void)
 	int n = 0; // indeks ćelije u x smjeru
 	int m = 0; // indeks ćelije u y smjeru
 
-	for (i = 0; i < Nw; i++) // inicijaliziramo početne pozicije svih šetača
-	{
-		x[i] = 0;
-		y[i] = 0;
-	}
+	// for (i = 0; i < Nw; i++) // inicijaliziramo početne pozicije svih šetača
+	// {
+	// 	x[i] = 0;
+	// 	y[i] = 0;
+	// }
+
+	int P[100][100]; // inicijaliziramo polje vjerojatnosti, mreža 100x100
+
+	// ako je su koordinate x i y šetaća u toj i toj ćeliji, onda P[int(x)][int(y)]++; ZA SVAKI STOTI KORAK; ubače.
+
+	// 1. svaki walker napravi svoj korak 2. svaki 100ti korak se bilježi u P arrayu lokacija++ 1.
 
 	// inicijaliziramo polje vjerojatnosti, početne vrijednosti su 0
-	for (n = 0; n < 10; n++)
+	for (n = 0; n < 100; n++)
 	{
-		for (m = 0; m < 10; m++)
+		for (m = 0; m < 100; m++)
 		{
-			P[n * 10 + m] = 0;
+			P[n][m] = 0;
 		}
 	}
 
-	for (i = 1; i < Nk; i++) // za svaki korak
+	for (i = 0; i < Nk; i++) // za svaki korak
 	{
-		for (j = 0; j < Nw; j++) // za svakog walkera
+		// svakog walkera
+		for (j = 0; j < Nw; j++)
 		{
-
-			// ∆rx ∈ [−0.5, 0.5] ⊂ R i ∆ry ∈ [−1.5, 1.5] ⊂ R
+			// pomakni walkera x i y, dakle imat ćeš arrayeve x i y za sve šetače u tom trenutku
 			x[j] = x[j] + ran1(&idum) - 0.5;
 			y[j] = y[j] + 3 * ran1(&idum) - 1.5;
 
 			// Ω = { (x, y) : | x − 50| ≤ 50, |y − 50| ≤ 50 } ⊂ R
 			if (x[j] > 100)
-				x[j] = 200 - x[j];
+				x[j] = 200 - x[j]; // jer 100-(x-100) = 200-x
 			if (y[j] > 100)
 				y[j] = 200 - y[j];
 			if (x[j] < 0)
@@ -60,32 +69,37 @@ int main(void)
 			if (y[j] < 0)
 				y[j] = -1 * y[j];
 
-			if (Nk % k == 0) // za svaki stoti korak, za svakog walkera
+			if (i % k == 0)
 			{
-				// provjeri za svaku kućicu (n i m)
-				for (n = 0; n < 10; n++)
+
+				n = (int)floor(x[j]);					 // možda n-1 stavit u P?
+				m = (int)floor(y[j]);					 // možda m-1 u P?
+				printf("%f %f\n", x[j], y[j]); // od
+				printf("%d %d\n", n, m);
+				if (n == 100)
 				{
-					for (m = 0; m < 10; m++)
-					{
-						// ako su x i y unutar granica n-tog odnosno m-tog intervala P++
-						if ((float)n <= x[i] && x[i] < (float)(n + 1))
-						{
-							if ((float)m <= y[i] && y[i] < (float)(m + 1))
-							{
-								P[n * 10 + m]++;
-							}
-						}
-					}
+					P[n - 1][m]++;
 				}
+				else if (m == 100)
+				{
+					P[n][m - 1]++;
+				}
+				else
+				{
+					P[n][m]++;
+				}
+				printf("%d\n", P[n][m]);
 			}
 		}
 	}
 
-	for (i = 0; i < Nk; i = i + 100) // za svaki korak
+	for (n = 0; n < 100; n++)
 	{
-		for (j = 0; j < Nw; j++) // za svakog walkera
+		for (m = 0; m < 100; m++)
 		{
-			fprintf(file, "%f %f %d\n", x[j], y[j], P[(int)x[j] * 10 + (int)y[j]]);
+			// P[n][m] = 0;
+			// fprintf(file, "%f %f %d\n", x[j], y[j], (int)x[j] * 10 + (int)y[j]);
+			fprintf(file, "%d %d %d\n", n, m, P[n][m]);
 		}
 	}
 
