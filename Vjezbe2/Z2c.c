d5d27d43c14303f45c6cf486d9f69fc2c4f1651c
Ana Cavar <ana.cavar22@gmail.com>
2023-04-23 21:43:15 +0200

    ispravljen zadatak 2c

--- Vjezbe2/Z2c.c
+++ Vjezbe2/Z2c.c
@@ -3,25 +3,15 @@
 #include <math.h>
 #include "ran1.c"
 
-// Podijelite Ω na kvadratnu mrežu i odredite vjerojatnost P da se cestica nađe u pojedinoj ćeliji
-// mreže. Analizirajte ponašanje sustava crtajući razdiobe Nw = 250 000 šetaća za nekoliko
-// odabranih k = t/∆t
-
-// mreža je od 0 do 100 u x i u y smjeru
-
 int main(void)
 {
 	int i, j;
 	long idum = -1234;
-	// float x[250000]; // x koordinata svakog šetača
-	float x[10]; // x koordinata svakog šetača
-	// float y[250000]; // y koordinata svakog šetača
-	float y[10]; // y koordinata svakog šetača
-	// int Nw = 250000; // broj šetača
-	int Nw = 10; // broj šetača
-	// int Nk = 5000; // ŠTO ĆE MI OVAJ? broj koraka?
-	int Nk = 1000; // ŠTO ĆE MI OVAJ? broj koraka?
-	int k = 100;	 // k = t / Dt
+	float x[250000]; // x koordinata svakog šetača
+	float y[250000]; // y koordinata svakog šetača
+	int Nw = 250000; // broj šetača
+	int Nk = 5000;	 // broj koraka
+	int k = 100;		 // k = t / Dt
 
 	FILE *file;
 	file = fopen("Z2c.txt", "w");
@@ -29,19 +19,14 @@ int main(void)
 	int n = 0; // indeks ćelije u x smjeru
 	int m = 0; // indeks ćelije u y smjeru
 
-	// for (i = 0; i < Nw; i++) // inicijaliziramo početne pozicije svih šetača
-	// {
-	// 	x[i] = 0;
-	// 	y[i] = 0;
-	// }
+	for (i = 0; i < Nw; i++) // inicijaliziramo početne pozicije svih šetača
+	{
+		x[i] = 50;
+		y[i] = 50;
+	}
 
 	int P[100][100]; // inicijaliziramo polje vjerojatnosti, mreža 100x100
 
-	// ako je su koordinate x i y šetaća u toj i toj ćeliji, onda P[int(x)][int(y)]++; ZA SVAKI STOTI KORAK; ubače.
-
-	// 1. svaki walker napravi svoj korak 2. svaki 100ti korak se bilježi u P arrayu lokacija++ 1.
-
-	// inicijaliziramo polje vjerojatnosti, početne vrijednosti su 0
 	for (n = 0; n < 100; n++)
 	{
 		for (m = 0; m < 100; m++)
@@ -52,10 +37,8 @@ int main(void)
 
 	for (i = 0; i < Nk; i++) // za svaki korak
 	{
-		// svakog walkera
-		for (j = 0; j < Nw; j++)
+		for (j = 0; j < Nw; j++) // za svakog walkera
 		{
-			// pomakni walkera x i y, dakle imat ćeš arrayeve x i y za sve šetače u tom trenutku
 			x[j] = x[j] + ran1(&idum) - 0.5;
 			y[j] = y[j] + 3 * ran1(&idum) - 1.5;
 
@@ -71,11 +54,9 @@ int main(void)
 
 			if (i % k == 0)
 			{
+				n = (int)floor(x[j]);
+				m = (int)floor(y[j]);
 
-				n = (int)floor(x[j]);					 // možda n-1 stavit u P?
-				m = (int)floor(y[j]);					 // možda m-1 u P?
-				printf("%f %f\n", x[j], y[j]); // od
-				printf("%d %d\n", n, m);
 				if (n == 100)
 				{
 					P[n - 1][m]++;
@@ -88,7 +69,6 @@ int main(void)
 				{
 					P[n][m]++;
 				}
-				printf("%d\n", P[n][m]);
 			}
 		}
 	}
@@ -97,8 +77,6 @@ int main(void)
 	{
 		for (m = 0; m < 100; m++)
 		{
-			// P[n][m] = 0;
-			// fprintf(file, "%f %f %d\n", x[j], y[j], (int)x[j] * 10 + (int)y[j]);
 			fprintf(file, "%d %d %d\n", n, m, P[n][m]);
 		}
 	}
