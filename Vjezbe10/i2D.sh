feeea18695424ca31f49522bef4755b301ffc8cf
Ana Cavar <ana.cavar22@gmail.com>
2023-06-21 09:40:40 +0200

    Vjezbe10

---
+++ Vjezbe10/i2D.sh
@@ -0,0 +1,5 @@
+mkdir $TMPDIR/Vjezbe10
+cp -R /home/acavar/Vjezbe10/*  $TMPDIR/Vjezbe10
+cd  $TMPDIR/Vjezbe10
+/home/acavar/Vjezbe10/klasicni_sustav.exe
+cp -R $TMPDIR/Vjezbe10/*.dat  /home/acavar/Vjezbe10/
